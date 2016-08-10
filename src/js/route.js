05023644391b0b6f635e348f60f343fce1bda411
kyle <kyle.goines@gmail.com>
2016-08-10 10:22:16 -0400

    route js if i add routing

---
+++ src/js/route.js
@@ -0,0 +1,21 @@
+// /*
+// 	Shopping app config
+// */
+// import testCtrl from './testComponent.js';
+// console.log(testCtrl)
+
+// var routeCtrl = function( $routeProvider, $locationProvider ) {
+
+// 	$routeProvider
+// 		.when('/book/', {
+// 			templateUrl: 'templates/book.html',
+// 			controller: testCtrl
+// 		})
+// 		.when('/', {
+// 			templateUrl: 'templates/list.html',
+// 			controller: testCtrl
+// 		})
+
+// }
+
+// export default routeCtrl;
