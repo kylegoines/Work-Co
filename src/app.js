19a7104ce1360a66ccc34d198e8de8766abeb680
kyle <kyle.goines@gmail.com>
2016-08-10 10:18:16 -0400

    adding controllers to app

--- src/app.js
+++ src/app.js
@@ -2,3 +2,13 @@ require('./scss/main.scss');
 
 import angular from 'angular';
 import ngRoute from 'angular-route';
+
+import cartModule from './js/controllers/cartCtrl.js';
+import storeModule from './js/controllers/storeCtrl.js';
+
+import dataService from './js/services/dataService.js';
+
+angular.module( 'shoppingApp', [] )
+			.factory( 'dataService', dataService )
+			.controller( 'cartCtrl', ['$scope','dataService', cartModule] )
+			.controller( 'storeCtrl', ['$scope', 'dataService', storeModule] )
