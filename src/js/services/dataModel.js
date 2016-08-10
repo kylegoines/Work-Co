9b512332a3e8174682ea5744c786f2bafeb46555
kyle <kyle.goines@gmail.com>
2016-08-10 10:20:27 -0400

    adding data

---
+++ src/js/services/dataModel.js
@@ -0,0 +1,16 @@
+var dataModel = [
+	{
+		name: 'item 1',
+		price: 5.99,
+		quanity: 20,
+		tags: ['cheap', 'awesome'],
+	},
+	{
+		name: 'item 2',
+		price: 99.99,
+		quanity: 2,
+		tags: ['expensive', 'awesome'],
+	},
+]
+
+export default dataModel;
